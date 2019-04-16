26a6f7355d3a7e68b14f64a366433be7e8c54c84
Zaheer Moola <zmoola22@gmail.com>
2019-04-16 16:37:02 +0200

    Changes

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -9,7 +9,7 @@ public func routes(_ router: Router) throws {
     
     // Basic "Hello, world!" example
     router.get("hello") { req in
-        return "Hello, world!"
+        return "Hello, Zaheer!"
     }
     
 
