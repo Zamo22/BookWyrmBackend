5f3f79c85dfa4a577f28e3ce106965bce8322907
Zaheer Moola <zmoola22@gmail.com>
2019-04-16 16:32:20 +0200

    Added route to get isbns of current popular books

--- Sources/App/Controllers/TodoController.swift
+++ Sources/App/Controllers/TodoController.swift
@@ -20,4 +20,42 @@ final class TodoController {
             return todo.delete(on: req)
         }.transform(to: .ok)
     }
+    
+    
+    func getPopularBooks(req: Request) throws -> Future<PopularBooks> {
+        let urlToSearch = "http://idreambooks.com/api/publications/recent_recos.json?key=64f959b1d802bf39f22b52e8114cace510662582&slug=bestsellers"
+        let client = try req.make(Client.self)
+        let ans =  client.get(urlToSearch).flatMap { exampleResponse in
+            return try exampleResponse.content.decode([PopularBook].self)
+        }
+        
+        return ans.flatMap { model in
+            let all = PopularBooks(books: model)
+            return Future.map(on: req) {
+                return all }
+        }
+    }
+
+    func getBooks(req: Request) throws -> Future<Isbns> {
+        let popularBookList = try getPopularBooks(req: req)
+
+        return popularBookList.map(to: Isbns.self) { list in
+            let bookList = try PopularBooks(books: list.books)
+            var isbnList: [String] = []
+            for book in bookList.books {
+                let singleBookIsbns = book.isbns
+                let isbnListArr: [String] = singleBookIsbns.components(separatedBy: ",")
+                isbnList.append(isbnListArr.first ?? "123")
+            }
+            let isbns = try Isbns(isbnArray: isbnList)
+            return isbns
+        }
+
+
+    }
+    
+}
+
+struct Isbns: Codable, Content {
+    var isbnArray: [String]
 }
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -11,10 +11,21 @@ public func routes(_ router: Router) throws {
     router.get("hello") { req in
         return "Hello, world!"
     }
+    
 
     // Example of configuring a controller
     let todoController = TodoController()
     router.get("todos", use: todoController.index)
+    router.get("popular" , use: todoController.getBooks)
     router.post("todos", use: todoController.create)
     router.delete("todos", Todo.parameter, use: todoController.delete)
+}
+
+
+struct PopularBook: Codable, Content {
+    var isbns: String
+}
+
+struct PopularBooks: Codable, Content {
+    var books: [PopularBook]
 }
